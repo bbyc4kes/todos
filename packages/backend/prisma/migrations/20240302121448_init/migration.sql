6be832515db649501864470fca9ba396650ca81d
Max <azimov.workspace@gmail.com>
2024-04-30 14:26:41 +0200

    added successful migration for todo table

--- packages/backend/prisma/migrations/20240302121448_init/migration.sql
+++ packages/backend/prisma/migrations/20240302121448_init/migration.sql
@@ -1,19 +1,11 @@
+-- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "email" TEXT UNIQUE NOT NULL,
-    "name" VARCHAR(15),
+    "email" TEXT NOT NULL,
+    "name" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
-CREATE TABLE "Todo" (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" INTEGER NOT NULL REFERENCES "User"("id"),
-    "task" TEXT NOT NULL,
-
-    CONSTRAINT "Todo_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id")
-);
-
-CREATE UNIQUE INDEX "Todo_user_text_unique" ON "Todo"("user_id", "task");
