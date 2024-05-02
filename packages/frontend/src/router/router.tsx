f4e75b4d625a3501bc20a2f2528a1374fc879179
Max <azimov.workspace@gmail.com>
2024-05-02 16:58:11 +0200

    added protected route

--- packages/frontend/src/router/router.tsx
+++ packages/frontend/src/router/router.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
 import App from '~modules/app/app.module';
 
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route path="test" element={<h1>testing</h1>}></Route>
+		</Route>,
+	),
+);
+
 const Router: React.FunctionComponent = () => {
-	return (
-		// Implement Routes
-		<App />
-	);
+	return <RouterProvider router={router} />;
 };
 
 export default Router;
