5cf850a2e459e7fd24c75e41f8846c80169ea07d
Max <azimov.workspace@gmail.com>
2024-05-02 17:28:07 +0200

    fix: refactoring todo.controller.ts

--- packages/backend/src/controllers/todo.controller.ts
+++ packages/backend/src/controllers/todo.controller.ts
@@ -1,80 +1,74 @@
+import { ExpressFunctionType } from '@/types/todos.type';
 import todoService from '@/services/todo.service';
-import { ExpressFuncitonType } from '@/types/todos.type';
 
-const getAllTodos: ExpressFuncitonType = async (req, res) => {
-	const todos = await todoService.findAll();
-	return res.json(todos);
-};
+class TodoController {
+	getAllTodos: ExpressFunctionType = async (req, res) => {
+		const todos = await todoService.findAll();
+		return res.json(todos);
+	};
 
-const getTodoById: ExpressFuncitonType = async (req, res) => {
-	const { id } = req.params;
+	getTodoById: ExpressFunctionType = async (req, res) => {
+		const { id } = req.params;
 
-	const parsedId = parseFloat(id);
-	const todo = await todoService.findById(parsedId);
+		const parsedId = parseFloat(id);
+		const todo = await todoService.findById(parsedId);
 
-	return res.json(todo);
-};
+		return res.json(todo);
+	};
 
-const addNewTodo: ExpressFuncitonType = async (req, res) => {
-	const todoData = req.body;
-	const newTodo = await todoService.create(todoData);
-	return res.json(newTodo);
-};
+	addNewTodo: ExpressFunctionType = async (req, res) => {
+		const todoData = req.body;
+		const newTodo = await todoService.create(todoData);
+		return res.json(newTodo);
+	};
 
-const updateTodo: ExpressFuncitonType = async (req, res) => {
-	const { id } = req.params;
+	updateTodo: ExpressFunctionType = async (req, res) => {
+		const { id } = req.params;
 
-	const parsedId = parseFloat(id);
-	const todoData = req.body;
-	const updatedTodo = await todoService.update(parsedId, todoData);
-	return res.json(updatedTodo);
-};
+		const parsedId = parseFloat(id);
+		const todoData = req.body;
+		const updatedTodo = await todoService.update(parsedId, todoData);
+		return res.json(updatedTodo);
+	};
 
-const destroyTodo: ExpressFuncitonType = async (req, res) => {
-	const { id } = req.params;
+	destroyTodo: ExpressFunctionType = async (req, res) => {
+		const { id } = req.params;
 
-	const parsedId = parseFloat(id);
-	const deletedTodo = await todoService.destroy(parsedId);
+		const parsedId = parseFloat(id);
+		const deletedTodo = await todoService.destroy(parsedId);
 
-	return res.json(deletedTodo);
-};
+		return res.json(deletedTodo);
+	};
 
-const updateCompleteness: ExpressFuncitonType = async (req, res) => {
-	const { id } = req.params;
-	const { isCompleted } = req.query;
+	updateCompleteness: ExpressFunctionType = async (req, res) => {
+		const { id } = req.params;
+		const { isCompleted } = req.query;
 
-	const parsedId = parseFloat(id);
-	const todoIsCompleted = isCompleted === 'true';
+		const parsedId = parseFloat(id);
+		const todoIsCompleted = isCompleted === 'true';
 
-	const updatedTodo = await todoService.updateIsComplete(
-		parsedId,
-		todoIsCompleted,
-	);
+		const updatedTodo = await todoService.updateIsComplete(
+			parsedId,
+			todoIsCompleted,
+		);
 
-	return res.json(updatedTodo);
-};
+		return res.json(updatedTodo);
+	};
 
-const updatePrivacy: ExpressFuncitonType = async (req, res) => {
-	const { id } = req.params;
-	const { isPrivate } = req.query;
+	updatePrivacy: ExpressFunctionType = async (req, res) => {
+		const { id } = req.params;
+		const { isPrivate } = req.query;
 
-	const parsedId = parseFloat(id);
-	const todoIsPrivate = isPrivate === 'true';
+		const parsedId = parseFloat(id);
+		const todoIsPrivate = isPrivate === 'true';
 
-	const updatedTodo = await todoService.updateIsPublic(
-		parsedId,
-		todoIsPrivate,
-	);
+		const updatedTodo = await todoService.updateIsPublic(
+			parsedId,
+			todoIsPrivate,
+		);
 
-	return res.json(updatedTodo);
-};
+		return res.json(updatedTodo);
+	};
+}
 
-export default {
-	getAllTodos,
-	getTodoById,
-	addNewTodo,
-	updateTodo,
-	updateCompleteness,
-	updatePrivacy,
-	destroyTodo,
-};
+export default new TodoController();
