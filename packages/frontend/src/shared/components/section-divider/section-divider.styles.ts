20451331b1894b3d1f5c57aa518e7dd7ad7bc1af
Max <azimov.workspace@gmail.com>
2024-05-05 21:12:59 +0200

    added screen-size based utils

---
+++ packages/frontend/src/shared/components/section-divider/section-divider.styles.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/css';
+
+export const sectionDividerStyles = css`
+	background-color: #000;
+	margin-top: 6rem;
+	margin-bottom: 2rem;
+	height: 4rem;
+	width: 0.25rem;
+	border-radius: 9999px;
+`;
