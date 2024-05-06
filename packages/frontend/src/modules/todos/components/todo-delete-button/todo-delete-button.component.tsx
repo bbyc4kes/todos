49de5167b68aa43e4b6f2010ca1f4c38b747d683
Max <azimov.workspace@gmail.com>
2024-05-06 20:50:30 +0200

    fix: deleted unnecessary inline functions

--- packages/frontend/src/modules/todos/components/todo-delete-button/todo-delete-button.component.tsx
+++ packages/frontend/src/modules/todos/components/todo-delete-button/todo-delete-button.component.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useTodoStore } from '~store/todos/todo.store';
 import CustomButton from '~shared/components/button/button.component';
-import { deleteButtonStyles } from './todo-delete-button';
+import { deleteButtonStyles } from './todo-delete-button.styles';
 
-export default function DeleteButton({ id }): React.JSX.Element {
+export default function DeleteButton({
+	id,
+}: {
+	id: number;
+}): React.JSX.Element {
 	const { destroyTodo } = useTodoStore();
+
+	const handleDeletion = (): void => {
+		destroyTodo(id);
+	};
+
 	return (
 		<CustomButton
 			extraButtonStyles={deleteButtonStyles}
-			onClick={() => destroyTodo(id)}
+			onClick={handleDeletion}
 			text="Delete"
 		/>
 	);
