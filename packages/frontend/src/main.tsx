25908bd1146c114dd6c61e2ff989296f2f7940d1
Max <azimov.workspace@gmail.com>
2024-05-16 01:04:44 +0200

    added authprovider on frontend

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -5,11 +5,15 @@ import './shared/styles/global-styles.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import Router from './router/router';
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from 'react-auth-kit/AuthProvider';
+import { store } from '~store/user/user.store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<PortalProvider portalClassName="my-custom-class">
-		<BrowserRouter basename="">
-			<Router />
-		</BrowserRouter>
+		<AuthProvider store={store}>
+			<BrowserRouter basename="">
+				<Router />
+			</BrowserRouter>
+		</AuthProvider>
 	</PortalProvider>,
 );
