f835dda506a2b037543365a52605c4385d893cdf
Max <azimov.workspace@gmail.com>
2024-05-16 21:35:16 +0200

    implemented auth middleware and user routes

--- packages/backend/src/routes/api/user.route.ts
+++ packages/backend/src/routes/api/user.route.ts
@@ -1,5 +1,6 @@
 import userController from '@/controllers/user.controller';
 import catchAsync from '@/middlewares/catchAsync';
+import doesExist from '@/middlewares/doesExist';
 import { authenticate } from '@/middlewares/isLoggedIn';
 import validateBody from '@/middlewares/validateBody';
 import { userSchema } from '@/schemas/user.schema';
@@ -7,20 +8,29 @@ import { Router } from 'express';
 
 const userRouter: Router = Router();
 
-userRouter.get('/logout', userController.logoutUser);
-userRouter.get('/protected-route', authenticate, (req, res) => {
-	res.json({ message: 'Protected route.' });
-});
+userRouter.route('/').get(catchAsync(userController.getAllUsers));
+
+userRouter
+	.route('/:id')
+	.get(doesExist('user'), catchAsync(userController.getUserById))
+	.delete(doesExist('user'), authenticate, userController.destroyTodo);
+
+userRouter.post(
+	'/register',
+	validateBody(userSchema),
+	userController.registerUser.bind(userController),
+);
 
 userRouter.post(
 	'/login',
 	validateBody(userSchema),
 	catchAsync(userController.logInUser),
 );
-userRouter.post(
-	'/register',
-	validateBody(userSchema),
-	userController.registerUser.bind(userController),
-);
+
+userRouter.get('/logout', userController.logoutUser);
+
+userRouter.get('/protected-route', authenticate, (req, res) => {
+	res.json({ message: 'Protected route.' });
+});
 
 export default userRouter;
--- packages/backend/src/services/user.service.ts
+++ packages/backend/src/services/user.service.ts
@@ -1,10 +1,41 @@
-import { PrismaClient, User } from '@prisma/client';
+import { TUser } from '@/types/user.types';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export default class UserService {
-	async findAll(): Promise<User[]> {
-		const users = await prisma.user.findMany();
-		return users;
-	}
-}
+const findAll = async (): Promise<TUser[]> => {
+	const users = await prisma.user.findMany();
+	return users;
+};
+
+const findById = async (id: number): Promise<TUser | null> => {
+	const userById = await prisma.user.findUnique({
+		where: {
+			id,
+		},
+	});
+	return userById;
+};
+
+export const create = async (data: Prisma.UserCreateInput): Promise<TUser> => {
+	const newUser = await prisma.user.create({
+		data,
+	});
+	return newUser;
+};
+
+const destroy = async (id: number): Promise<TUser | null> => {
+	const removedTodo = prisma.user.delete({
+		where: {
+			id,
+		},
+	});
+	return removedTodo;
+};
+
+export default {
+	findAllUsers: findAll,
+	findUserById: findById,
+	createUser: create,
+	destroyUser: destroy,
+};
